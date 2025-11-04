0507dd861548042d30c143448dca22b0ab07d04c
minghansun1 <minghansun@hotmail.com>
2025-11-04 00:38:00 -0500

    tests

---
+++ tests/basic.test.ts
@@ -0,0 +1,28 @@
+import validatePizza from "../src/validation.js";
+
+describe("basic", () => {
+  test("fail", () => {
+    const result = validatePizza({"heheheha": true });
+    expect(result.isPizza).toBe(false);
+  });
+
+  test("pass", () => {
+  const result = validatePizza({ size: 2, crust: "normal" });
+    expect(result.isPizza).toBe(true);
+    expect(result.pizza).toBeDefined();
+    if (result.pizza){
+      expect(result.pizza.size).toBe(2);
+      expect(result.pizza.crust).toBe("normal");
+    }
+  });
+
+  test("fail_crust", () => {
+    const result = validatePizza({ size: 2, crust: "thin" });
+    expect(result.isPizza).toBe(false);
+  });
+
+  test("fail_toppings", () => {
+    const result = validatePizza({ size: 2, crust:"normal", toppings: ["pepperoni", "pineapple"] });
+    expect(result.isPizza).toBe(false);
+  });
+});
